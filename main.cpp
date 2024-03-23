d04a900e795a9dee6eb247ef44d2b2ce43079d12
BobyWoby <bestdogeever865@gmail.com>
2024-03-22 23:50:45 -0500

    added particle hash array, yet to be implemented

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "Particle.h"
 #include "SPH.h"
 #include <random>
+#define GRID_COLS 40
+#define GRID_ROWS 30
 
 void velocityToColor(Particle *p, sf::Vector2f v){
     sf::Color c;
@@ -16,7 +18,29 @@ void velocityToColor(Particle *p, sf::Vector2f v){
     c.b = 200.0 - vMag / 5.0 * 255.0;
     p->setColor(c);
 }
-
+void updateParticleHash(Particle *particles, int numParticles , std::pair<int, int> *particleHash, std::array<int, GRID_COLS * GRID_ROWS> &startIndices){
+    startIndices.fill(-1);
+    for(int i = 0; i < numParticles; i++){
+        int x = (int)particles[i].getPosition().x / (800 / GRID_COLS);
+        int y = (int)particles[i].getPosition().y / (600 / GRID_ROWS);
+        int cellIndex = y * GRID_COLS + x;
+        particleHash[i] = std::make_pair(i, cellIndex);
+    }
+    //sort particleHash by particle cell index
+    for(int i = 0; i < numParticles; i++){
+        for(int j = 0; j < numParticles; j++){
+            if(particleHash[j].first > particleHash[i].first){
+                std::swap(particleHash[j], particleHash[i]);
+            }
+        }
+    }
+    //find start indices
+    for(int i = 0; i < numParticles; i++){
+        if(particleHash[i].first != -1 && startIndices[particleHash[i].second] != i){
+            startIndices[particleHash[i].second] = i;
+        }
+    }
+}
 int main() {
     const int numParticles = 600;
     const int numCols = 20;
@@ -25,6 +49,12 @@ int main() {
     const float gravity = 0;
     const int framerate = 60;
     float gasConstant = 100;
+
+
+    // particleId, gridIndex
+    std::pair<int, int> particleHash[numParticles];
+    int startIndices[numParticles];
+
     sf::Font font;
     if(!font.loadFromFile("UbuntuMono-R.ttf")){
         std::cout << "Couldn't load font";
@@ -103,7 +133,7 @@ int main() {
         MousePos = sf::Mouse::getPosition(window);
         circle.setPosition(MousePos.x, MousePos.y);
         density = sph.getDensity(MousePos.x, MousePos.y, particles);
-        str.setString("Density at Mouse: " + std::to_string(density) + "\nGas Constant: " + std::to_string(gasConstant));
+        str.setString("Density at Mouse: " + std::to_string(density) + "\nTemperature: " + std::to_string(gasConstant));
         sph.updateDensities(particles);
         for(auto & particle : particles){
             //calculate pressure and add the acceleration to the velocity
